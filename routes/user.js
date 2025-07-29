5863d021c566640a4b6ef5b0040edcb6c030a6c0
govindjadhav <jgovind155@gmail.com>
2025-07-30 01:25:40 +0530

    logout and stay on current page is done

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const passport = require("passport");
-
+const { savedRedirectUrl } = require("../middelware.js")
 
 // Render the signup page
 router.get("/signup", (req, res) => {
@@ -30,16 +30,18 @@ router.post("/signup", async (req, res) => {
 
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
-});// Handle login form submission
+});
+// Handle login form submission
 router.post(
     "/login",
+    savedRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: "/login",
         failureFlash: true
     }),
-    (req, res) => {
+    async (req, res) => {
         req.flash("success", `Welcome again, ${req.user.username}!`);
-        res.redirect("/listings");
+        res.redirect(res.locals.redirectUrl);
     }
 );
 //logout
